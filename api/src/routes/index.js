45269fba2c9514bd8480d7ff353c712384f3ff2f
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-04-14 07:38:18 -0500

    back done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const axios = require('axios');
@@ -7,6 +8,7 @@ const { APIKEY } = process.env;
 const { Dog, Temperament } = require('../db')
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
 
 
 //Funciones para el GET de dogs, dogs query y dogs params
@@ -28,7 +30,7 @@ const getApiInfo = async () => {
 const getDbInfo = async () => {
     return await Dog.findAll({
         include: {
-            model: Temperament, 
+            model: Temperament,
             attributes: ['temperament'],
             through: {
                 attributes: [],
@@ -60,69 +62,92 @@ const getAllTemperaments = async () => {
 router.get('/dogs', async (req, res) => {
     const nameQuery = req.query.name
     const allDogs = await getAllInfo();
-    if(nameQuery){
-        const dogName = await allDogs.filter( dog => dog.name.toLowerCase().includes(nameQuery.toLowerCase()))
+    if (nameQuery) {
+        const dogName = await allDogs.filter(dog => dog.name.toLowerCase().includes(nameQuery.toLowerCase()))
         dogName.length ?
-        res.status(200).send(dogName) :
-        res.status(404).send('No esta el nombre')
-    }else {
+            res.status(200).send(dogName) :
+            res.status(404).send('No esta el nombre')
+    } else {
         res.status(200).send(allDogs)
     }
 })
 
 //ruta de dogs params
-router.get('/dogs/:dog', async (req, res) => {
-    const nameParams = req.params.dog
+router.get('/dogs/:idDog', async (req, res) => {
+    const id = req.params.idDog
     const allDogs = await getAllInfo();
-    if(nameParams){
-        const dogName = await allDogs.filter( dog => dog.name.toLowerCase().includes(nameParams.toLowerCase()))
-        dogName.length ?
-        res.status(200).send(dogName) :
-        res.status(404).send('No esta el nombre')
-    }else {
-        res.status(200).send(allDogs)
+    if (id) {
+        const dogId = await allDogs.filter(dog => dog.id == id)
+        dogId.length ?
+            res.status(200).json(dogId) :
+            res.status(404).send('No esta el id')
     }
 })
 
 
 
 //ruta de temperaments
-router.get('/temperament', async (req,res) => {
+router.get('/temperament', async (req, res) => {
     const temps = await getAllTemperaments();
     let allTemps = []
     let temperamentsArray = await temps.map((temp) => {
-      let temperaments = temp.temperament;
-      if (temperaments) {
-        temperaments = temperaments.split(", ");
-      }
-      return temperaments;
+        let temperaments = temp.temperament;
+        if (temperaments) {
+            temperaments = temperaments.split(", ");
+        }
+        return temperaments;
     });
-    for(let i = 0; i < temperamentsArray.length; i++){
-      allTemps = allTemps.concat(temperamentsArray[i])
-   }
-//    allTemps.forEach(temp => {
-//        Temperament.findOrCreate({
-//            where: {temperament: allTemps}
-//        })
-//    })
-for(let h = 0; h < allTemps.length - 1; h++){
-   if(allTemps[h] != null){ 
-    let aux = allTemps[h]
-    Temperament.findOrCreate({
-               where: {temperament: aux}
-         })
+    for (let i = 0; i < temperamentsArray.length; i++) {
+        allTemps = allTemps.concat(temperamentsArray[i])
     }
-}
-// Temperament.findOrCreate({
-//               where: {temperament: "test"}
-//          })
+    //    allTemps.forEach(temp => {
+    //        Temperament.findOrCreate({
+    //            where: {temperament: allTemps}
+    //        })
+    //    })
+    for (let h = 0; h < allTemps.length - 1; h++) {
+        if (allTemps[h] != null) {
+            let aux = allTemps[h]
+            Temperament.findOrCreate({
+                where: { temperament: aux }
+            })
+        }
 
-const allTemps2 = await Temperament.findAll();
+    }
+    // Temperament.findOrCreate({
+    //               where: {temperament: "test"}
+    //          })
+
+
+    const allTemps2 = await Temperament.findAll();
 
     res.status(200).send(allTemps2)
 })
 
 
+router.post('/dog', async (req, res) => {
+    let {
+        name,
+        height,
+        weight,
+        life_span,
+        temperament
+    } = req.body
+
+   let newDog = await Dog.create({
+        name,
+        height,
+        weight,
+        life_span,
+    })
+
+
+    let temperamentDb = await Temperament.findAll({
+        where: { temperament: temperament }
+    })
+    newDog.addTemperament(temperamentDb)
+    res.send('todo ok')
+});
 
 
 module.exports = router;
